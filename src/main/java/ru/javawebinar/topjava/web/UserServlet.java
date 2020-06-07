f850bd85148269d4d189d055c8b0cde6e1d3ba24
knastnt <456kot@mail.ru>
2020-06-07 11:52:05 +1000

    1_2_switch_to_war

---
+++ src/main/java/ru/javawebinar/topjava/web/UserServlet.java
@@ -0,0 +1,13 @@
+package ru.javawebinar.topjava.web;
+
+import java.io.IOException;
+
+public class UserServlet extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        System.out.println("post " + request.toString());
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        System.out.println("get " + request.toString());
+    }
+}
