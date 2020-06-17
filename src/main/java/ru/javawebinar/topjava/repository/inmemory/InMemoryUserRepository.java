5b0c13dc03864fd5d6d11ac1ac4b109f34d8e4ef
knastnt <456kot@mail.ru>
2020-06-17 11:32:31 +1000

    InMemoryUserRepository implements UserRepository

--- src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryUserRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryUserRepository.java
@@ -6,40 +6,57 @@ import org.springframework.stereotype.Repository;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.UserRepository;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 
 @Repository
 public class InMemoryUserRepository implements UserRepository {
     private static final Logger log = LoggerFactory.getLogger(InMemoryUserRepository.class);
 
+    private Map<Integer, User> repository = new ConcurrentHashMap<>();
+    private AtomicInteger idGenerator = new AtomicInteger();
+
     @Override
     public boolean delete(int id) {
         log.info("delete {}", id);
-        return true;
+        return repository.remove(id) != null;
     }
 
     @Override
     public User save(User user) {
         log.info("save {}", user);
+        Objects.requireNonNull(user);
+        if (user.isNew()){
+            user.setId(idGenerator.incrementAndGet());
+        }
+        repository.put(user.getId(), user);
         return user;
     }
 
     @Override
     public User get(int id) {
         log.info("get {}", id);
-        return null;
+        //Проверить совпадение id внутри User
+        return repository.get(id);
     }
 
     @Override
     public List<User> getAll() {
         log.info("getAll");
-        return Collections.emptyList();
+        return new ArrayList<>(repository.values());
     }
 
     @Override
     public User getByEmail(String email) {
         log.info("getByEmail {}", email);
-        return null;
+        Objects.requireNonNull(email);
+        List<User> filteredByEmail = repository.values().stream()
+                .filter(user -> user.getEmail().equalsIgnoreCase(email))
+                .collect(Collectors.toList());
+        if (filteredByEmail.size() == 0) return null;
+        if (filteredByEmail.size() > 1) throw new RuntimeException("More than one User with required email");
+        return filteredByEmail.get(0);
     }
 }
