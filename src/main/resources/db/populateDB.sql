39682dbbb7a69c3c57f27e3b31960ef3a8ff6794
knastnt <456kot@mail.ru>
2020-06-23 17:10:41 +1000

    тут тупой коммент

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -11,4 +11,5 @@ INSERT INTO user_roles (role, user_id) VALUES
   ('ROLE_ADMIN', 100001);
 
 INSERT INTO meals (user_id, datetime, description, calories) VALUES
-  (100000, '2020-06-23 15:06:01', 'Описание еды', 15);
+  (100000, '2020-06-23 15:06:01', 'Описание еды', 15),
+  (100001, '2020-06-23 15:06:01', 'Админская еда', 30);
