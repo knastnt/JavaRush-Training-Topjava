57894cc865291ec2591ad4f8ae6bdd7154209a57
knastnt <456kot@mail.ru>
2020-06-23 16:01:46 +1000

    2 Дополнить скрипты создания и инициализации базы таблицой MEALS. Запустить скрипты на вашу базу (через Run). Порядок таблиц при DROP и DELETE важен, если они связаны внешними ключами (foreign key, fk). Проверьте, что ваши скрипты работают

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -1,4 +1,6 @@
 DROP TABLE IF EXISTS user_roles;
+DROP TABLE IF EXISTS meals;
+-- сначала удаляем таблицы user_roles и meals, и только потом - users, т.к. первая и вторая зависят от users
 DROP TABLE IF EXISTS users;
 DROP SEQUENCE IF EXISTS global_seq;
 
@@ -22,4 +24,14 @@ CREATE TABLE user_roles
   role    VARCHAR,
   CONSTRAINT user_roles_idx UNIQUE (user_id, role),
   FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+);
+
+CREATE TABLE meals
+(
+    id          INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
+    user_id     INTEGER                     NOT NULL,
+    dateTime    TIMESTAMP                   NOT NULL,
+    description VARCHAR                     NOT NULL,
+    calories    INTEGER                     NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -9,3 +9,6 @@ INSERT INTO users (name, email, password) VALUES
 INSERT INTO user_roles (role, user_id) VALUES
   ('ROLE_USER', 100000),
   ('ROLE_ADMIN', 100001);
+
+INSERT INTO meals (user_id, datetime, description, calories) VALUES
+  (100000, '2020-06-23 15:06:01', 'Описание еды', 15);
