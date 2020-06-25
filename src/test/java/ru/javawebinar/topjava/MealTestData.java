36a71079fe07552368054ae6999416c938d1f1ed
knastnt <456kot@mail.ru>
2020-06-25 13:19:34 +1000

    usingFieldByFieldElementComparator, иначе для сравнения используется equals объекта

--- src/test/java/ru/javawebinar/topjava/MealTestData.java
+++ src/test/java/ru/javawebinar/topjava/MealTestData.java
@@ -12,11 +12,18 @@ import static ru.javawebinar.topjava.model.AbstractBaseEntity.START_SEQ;
 public class MealTestData {
     public static final int NOT_FOUND_MEAL_ID = 99999;
     public static final int USER_MEAL_ID = START_SEQ + 2;
-    public static final int ADMIN_MEAL_ID = START_SEQ + 3;
+    public static final int ADMIN_MEAL1_ID = START_SEQ + 3;
 
     public static final LocalDate ONE_DAY = LocalDate.of(2020, 06, 23);
 
-    public static final Meal ADMIN_MEAL = new Meal(ADMIN_MEAL_ID, LocalDateTime.of(2020, 06, 23, 15,6,1), "Админская еда", 30);
+    public static final Meal USER_MEAL = new Meal(USER_MEAL_ID, LocalDateTime.of(2020, 06, 23, 15,6,1), "Описание еды", 15);
+
+    public static final Meal ADMIN_MEAL1 = new Meal(ADMIN_MEAL1_ID, LocalDateTime.of(2020, 06, 23, 15,6,1), "Админская еда", 30);
+    public static final Meal ADMIN_MEAL2 = new Meal(ADMIN_MEAL1_ID+1, LocalDateTime.of(2020, 06, 22, 23,59,59), "Админская еда прошлого дня", 150);
+    public static final Meal ADMIN_MEAL3 = new Meal(ADMIN_MEAL1_ID+2, LocalDateTime.of(2020, 06, 23, 0,0,0), "Админская еда в начале суток", 100);
+    public static final Meal ADMIN_MEAL4 = new Meal(ADMIN_MEAL1_ID+3, LocalDateTime.of(2020, 06, 23, 23,59,0), "Админская еда в 23:59:00", 1000);
+    public static final Meal ADMIN_MEAL5 = new Meal(ADMIN_MEAL1_ID+4, LocalDateTime.of(2020, 06, 23, 23,59,59), "Админская еда в 23:59:59", 870);
+    public static final Meal ADMIN_MEAL6 = new Meal(ADMIN_MEAL1_ID+5, LocalDateTime.of(2020, 06, 24, 13,0,5), "Админская еда следующего дня", 500);
 
     public static Meal getNew() {
         return new Meal(LocalDateTime.now(), "Now Meal Desc", 55);
@@ -38,6 +45,6 @@ public class MealTestData {
     }
 
     public static void assertMatch(Iterable<Meal> actual, Iterable<Meal> expected) {
-        assertThat(actual).usingDefaultElementComparator().isEqualTo(expected);
+        assertThat(actual).usingFieldByFieldElementComparator().isEqualTo(expected);
     }
 }
