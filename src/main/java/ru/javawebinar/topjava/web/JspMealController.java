68a267b28411b7c76d8f2a9c8de6fda09e441068
knastnt <456kot@mail.ru>
2020-07-24 13:26:33 +1000

    1.3.2 в одном контроллере нельзя использовать другой. Чтобы не дублировать код, можно сделать наследование контроллеров от абстрактного класса

--- src/main/java/ru/javawebinar/topjava/web/JspMealController.java
+++ src/main/java/ru/javawebinar/topjava/web/JspMealController.java
@@ -1,11 +1,13 @@
 package ru.javawebinar.topjava.web;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.util.DateTimeUtil;
+import ru.javawebinar.topjava.web.meal.MealAbstractController;
 import ru.javawebinar.topjava.web.meal.MealRestController;
 
 import java.time.LocalDate;
@@ -16,13 +18,16 @@ import java.time.temporal.ChronoUnit;
 
 @Controller
 @RequestMapping("/meals")
-public class JspMealController {
-    @Autowired
-    MealRestController mealController;
+public class JspMealController extends MealAbstractController {
+    static {
+        log = LoggerFactory.getLogger(JspMealController.class);
+    }
 
     @GetMapping
     public String list(FilterParams filterParams, Model model){
-        model.addAttribute("meals", mealController.getBetween(
+//        log.debug("Get /meals. " + filterParams); избыточно. это уже логируется стрингом
+
+        model.addAttribute("meals", getBetween(
                 filterParams.getStartDate(),
                 filterParams.getStartTime(),
                 filterParams.getEndDate(),
@@ -33,28 +38,34 @@ public class JspMealController {
 
     @GetMapping({"/{id}", "/new"})
     public String edit(@PathVariable(required = false) Integer id, Model model){
+//        log.debug("Get /meals/" + (id==null ? "new" : id)); избыточно. это уже логируется стрингом
+
         final Meal meal = id == null ?
                 new Meal(LocalDateTime.now().truncatedTo(ChronoUnit.MINUTES), "", 1000) :
-                mealController.get(id);
+                get(id);
         model.addAttribute("meal", meal);
         return "mealForm";
     }
 
     @PostMapping({"/{id}", "/new"})
     public String save(@PathVariable(required = false) Integer id, @RequestParam String dateTime, @RequestParam String description, @RequestParam Integer calories){
+//        log.debug("Post (saving) /meals/" + (id==null ? "new" : id)); избыточно. это уже логируется стрингом
+
         Meal meal = new Meal(LocalDateTime.parse(dateTime), description, calories);
 
         if (id == null) {
-            mealController.create(meal);
+            create(meal);
         } else {
-            mealController.update(meal, id);
+            update(meal, id);
         }
         return "redirect:/meals";
     }
 
     @PostMapping("/delete")
-    public String delete(@RequestParam Integer id){
-        mealController.delete(id);
+    public String remove(@RequestParam Integer id){
+        log.debug("Удаление Meals с id=" + id);
+
+        delete(id);
         return "redirect:/meals";
     }
 
@@ -94,6 +105,16 @@ public class JspMealController {
 
         public void setEndTime(String endTime) {
             this.endTime = endTime;
+        }
+
+        @Override
+        public String toString() {
+            return "FilterParams{" +
+                    "startDate='" + startDate + '\'' +
+                    ", endDate='" + endDate + '\'' +
+                    ", startTime='" + startTime + '\'' +
+                    ", endTime='" + endTime + '\'' +
+                    '}';
         }
     }
 }
