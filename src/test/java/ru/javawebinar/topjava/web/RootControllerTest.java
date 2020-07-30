65deb68609d11f88d2ced6c62dd5013bf895a7e9
knastnt <456kot@mail.ru>
2020-07-30 16:05:07 +1000

    1: Добавить тесты контроллеров:
        1.1 RootControllerTest.testMeals для meals.jsp
        1.2 Сделать ResourceControllerTest для style.css (проверить status и ContentType)

--- src/test/java/ru/javawebinar/topjava/web/RootControllerTest.java
+++ src/test/java/ru/javawebinar/topjava/web/RootControllerTest.java
@@ -2,13 +2,20 @@ package ru.javawebinar.topjava.web;
 
 import org.assertj.core.matcher.AssertionMatcher;
 import org.junit.jupiter.api.Test;
+import ru.javawebinar.topjava.MealTestData;
+import ru.javawebinar.topjava.TestMatcher;
+import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.model.User;
+import ru.javawebinar.topjava.to.MealTo;
+import ru.javawebinar.topjava.util.MealsUtil;
 
+import java.util.Arrays;
 import java.util.List;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static ru.javawebinar.topjava.MealTestData.*;
 import static ru.javawebinar.topjava.UserTestData.*;
 
 class RootControllerTest extends AbstractControllerTest {
@@ -28,5 +35,31 @@ class RootControllerTest extends AbstractControllerTest {
                             }
                         }
                 ));
+    }
+
+    @Test
+    void getAdminMeals() throws Exception {
+        checkMealsForUser(ADMIN, Arrays.asList(ADMIN_MEAL2, ADMIN_MEAL1));
+    }
+
+    @Test
+    void getUserMeals() throws Exception {
+        checkMealsForUser(USER, MEALS);
+    }
+
+
+    private void checkMealsForUser(User user, List<Meal> meals) throws Exception {
+        SecurityUtil.setAuthUserId(user.getId());
+        perform(get("/meals"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("meals"))
+                .andExpect(forwardedUrl("/WEB-INF/jsp/meals.jsp"))
+                .andExpect(model().attribute("meals", new AssertionMatcher<List<MealTo>>(){
+                    @Override
+                    public void assertion(List<MealTo> actual) throws AssertionError {
+                        TestMatcher.usingFieldsComparator(MealTo.class, "user").assertMatch(actual, MealsUtil.getTos(meals, user.getCaloriesPerDay()));
+                    }
+                }));
     }
 }
